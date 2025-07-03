ce1cca2d4ccf83452cf9560c20e044bbf293e784
AndrianinaH <rakotoandrianina@gmail.com>
2025-07-03 17:02:39 +0300

    [ORM] add drizzle

---
+++ drizzle/schema.ts
@@ -0,0 +1,13 @@
+import { pgTable, serial, text, timestamp, uniqueIndex } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+}, (users) => {
+  return {
+    uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+  };
+});
