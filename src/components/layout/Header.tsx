1434f926f584904e86e50f64db3de397c94dfd1f
AndrianinaH <rakotoandrianina@gmail.com>
2025-07-03 15:14:04 +0300

    hide menu for auth path

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Home, ListChecks, User } from "lucide-react";
 import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { MobileNav } from "./MobileNav";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isAuthRoute = pathname.startsWith("/auth");
+
   return (
     <header className="flex h-16 items-center justify-between px-4 md:px-6 border-b">
       <Link href="/" className="flex items-center gap-2 font-semibold">
@@ -13,27 +18,29 @@ export default function Header() {
         </span>
         <span className="text-lg text-bleu-950">Vola</span>
       </Link>
-      <nav className="hidden md:flex items-center gap-2 sm:gap-4">
-        <Button variant="ghost" asChild>
-          <Link href="/">
-            <Home className="h-4 w-4 mr-2" />
-            Dashboard
-          </Link>
-        </Button>
-        <Button variant="ghost" asChild>
-          <Link href="/movements">
-            <ListChecks className="h-4 w-4 mr-2" />
-            Mouvements
-          </Link>
-        </Button>
-        <Button variant="ghost" asChild>
-          <Link href="/profile">
-            <User className="h-4 w-4 mr-2" />
-            Profil
-          </Link>
-        </Button>
-        <ThemeToggle />
-      </nav>
+      {!isAuthRoute && (
+        <nav className="hidden md:flex items-center gap-2 sm:gap-4">
+          <Button variant="ghost" asChild>
+            <Link href="/">
+              <Home className="h-4 w-4 mr-2" />
+              Dashboard
+            </Link>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link href="/movements">
+              <ListChecks className="h-4 w-4 mr-2" />
+              Mouvements
+            </Link>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link href="/profile">
+              <User className="h-4 w-4 mr-2" />
+              Profil
+            </Link>
+          </Button>
+          <ThemeToggle />
+        </nav>
+      )}
       <div className="md:hidden">
         <MobileNav />
       </div>
--- src/components/layout/MobileNav.tsx
+++ src/components/layout/MobileNav.tsx
@@ -10,9 +10,12 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { ThemeToggle } from "@/components/ui/ThemeToggle";
+import { usePathname } from "next/navigation";
 
 export function MobileNav() {
   const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+  const isAuthRoute = pathname.startsWith("/auth");
 
   return (
     <Sheet open={open} onOpenChange={setOpen}>
@@ -24,32 +27,34 @@ export function MobileNav() {
       </SheetTrigger>
       <SheetContent side="left" className="flex flex-col">
         <SheetTitle className="sr-only">Menu de navigation</SheetTitle>
-        <nav className="grid gap-2 text-lg font-medium">
-          <Link
-            href="/"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-            onClick={() => setOpen(false)}
-          >
-            <Home className="h-5 w-5" />
-            Dashboard
-          </Link>
-          <Link
-            href="/movements"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-            onClick={() => setOpen(false)}
-          >
-            <ListChecks className="h-5 w-5" />
-            Mouvements
-          </Link>
-          <Link
-            href="/profile"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-            onClick={() => setOpen(false)}
-          >
-            <User className="h-5 w-5" />
-            Profil
-          </Link>
-        </nav>
+        {!isAuthRoute && (
+          <nav className="grid gap-2 text-lg font-medium">
+            <Link
+              href="/"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              onClick={() => setOpen(false)}
+            >
+              <Home className="h-5 w-5" />
+              Dashboard
+            </Link>
+            <Link
+              href="/movements"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              onClick={() => setOpen(false)}
+            >
+              <ListChecks className="h-5 w-5" />
+              Mouvements
+            </Link>
+            <Link
+              href="/profile"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              onClick={() => setOpen(false)}
+            >
+              <User className="h-5 w-5" />
+              Profil
+            </Link>
+          </nav>
+        )}
         <div className="mt-4 flex items-center gap-4 mx-[-0.65rem] px-3 py-2">
           <ThemeToggle />
           <span className="text-muted-foreground">Theme</span>
